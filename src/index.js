af9ad113f6b1dedf861e2d0d6d2031dd606b75d4
Alec Medeiros <alecmm@outlook.com>
2022-08-05 16:15:16 -0300

    Req 01 - Ok

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import App from './App';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={ store }>
+      <App />
+    </Provider>
   </BrowserRouter>,
 
   document.getElementById('root'),
