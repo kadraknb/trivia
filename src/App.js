04059f7acb55f429ff965048da7137aa6f249181
Alec Medeiros <alecmm@outlook.com>
2022-08-08 17:51:03 -0300

    Req18 - Ok

--- src/App.js
+++ src/App.js
@@ -8,12 +8,14 @@ import Login from './pages/Login';
 import Game from './pages/Game';
 import Settings from './pages/Settings';
 import Feedback from './pages/Feedback';
+import Ranking from './pages/Ranking';
 
 import './index.css';
 
 export default function App() {
   return (
     <Switch>
+      <Route path="/ranking" exact component={ Ranking } />
       <Route path="/feedback" exact component={ Feedback } />
       <Route path="/settings" exact component={ Settings } />
       <Route path="/game" exact component={ Game } />
