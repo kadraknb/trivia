2987be44e78104b1c37e1ec89597f3b150dabd3c
kadraknb <kadrak02@hotmail.com>
2022-08-08 19:39:03 -0300

    Requisito 19

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,18 +2,29 @@ import React from 'react';
 import propTypes from 'prop-types';
 import md5 from 'crypto-js/md5';
 import { connect } from 'react-redux';
+import { getIgmAction } from '../redux/actions';
 
 class Header extends React.Component {
-  render() {
-    const { gravatarEmail, score, name } = this.props;
+  getImg = (gravatarEmail) => {
+    const { dispatch } = this.props;
     const getHash = md5(gravatarEmail).toString();
     const img = `https://www.gravatar.com/avatar/${getHash}`;
+    dispatch(getIgmAction(img));
+    return img;
+  }
+
+  render() {
+    const { gravatarEmail, score, name } = this.props;
     return (
       <div>
         <header>
-          <img data-testid="header-profile-picture" alt="gravatar imagem" src={ img } />
-          <p data-testid="header-player-name">{ name }</p>
-          <p data-testid="header-score">{ score }</p>
+          <img
+            data-testid="header-profile-picture"
+            alt="gravatar imagem"
+            src={ this.getImg(gravatarEmail) }
+          />
+          <p data-testid="header-player-name">{name}</p>
+          <p data-testid="header-score">{score}</p>
         </header>
       </div>
     );
@@ -30,6 +41,7 @@ Header.propTypes = {
   gravatarEmail: propTypes.string.isRequired,
   score: propTypes.string.isRequired,
   name: propTypes.string.isRequired,
+  dispatch: propTypes.func.isRequired,
 };
 
 export default connect(mapStateToProps)(Header);
--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -4,19 +4,45 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { playerPerformance } from '../redux/actions';
 
-import Header from '../components/Header';
-
 class Ranking extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      rankings: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ rankings: JSON.parse(localStorage.getItem('ranking')) });
+  }
+
   goHome = () => {
     const { history } = this.props;
     history.push('/');
   }
 
+  sortRankings = () => {
+    const { rankings } = this.state;
+    rankings.sort((a, b) => b[2] - a[2]);
+    console.log(rankings);
+    return rankings.map((rank, ii) => (
+      <li key={ ii }>
+        <img src={ rank[0] } alt="foto do usuario" />
+        <h4 data-testid={ `player-name-${ii}` }>{rank[1]}</h4>
+        <h3 data-testid={ `player-score-${ii}` }>{rank[2]}</h3>
+      </li>
+    ));
+  }
+
   render() {
+    const { rankings } = this.state;
+    console.log(rankings);
     return (
       <div>
-        <Header />
         <h1 data-testid="ranking-title">Ranking</h1>
+        <ol>
+          { this.sortRankings() }
+        </ol>
         <button
           type="button"
           data-testid="btn-go-home"
