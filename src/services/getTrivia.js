2131ffc41e861fce2df78143c83adc34ea0c4671
kadranb <kadrak02@hotmail.com>
2022-12-18 13:54:18 -0300

    build clean

--- src/services/getTrivia.js
+++
@@ -1,8 +0,0 @@
-const getTrivia = async (token) => {
-  const TRIVIA_API = `https://opentdb.com/api.php?amount=5&token=${token}`;
-  const getApiData = await fetch(TRIVIA_API);
-  const trivia = await getApiData.json();
-  return trivia;
-};
-
-export default getTrivia;
